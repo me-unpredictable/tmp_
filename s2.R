2e4f83e7ded891882ee9ca65920b8c59ca59cd4e
meunpredictable <vishalpatel654@gmail.com>
2024-05-15 12:30:43 +1000

    New plot in new branch

--- s2.R
+++ s2.R
@@ -3,7 +3,11 @@ data=read.csv('epiSEIHCRD_combAge.csv')
 data$t=as.Date(data$t,origin='2020-3-01')
 data_plot=subset(data,subset = (data$t>='2020-07-01' & data$t<='2020-12-31'))
 # Update axis breaks to months
-ggplot(data_plot, aes(x = t, y = H)) +
-  geom_line(color='red') +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b") +labs(x='Months',y='Beds',title='Covid-19 Bed Requirments')
+ggplot(data_plot) +
+  geom_line(aes(x = t, y = H),color='red') +
+  geom_line(aes(x=t,y=D),color='blue')+
+  geom_line(aes(x=t,y=C),color='green')+
+  scale_y_sqrt()+
+  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+  labs(x='Months',y='Beds',title='Covid-19 Bed Requirments')
 
